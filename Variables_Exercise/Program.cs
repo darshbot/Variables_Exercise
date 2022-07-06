6ee837a5453b1de04ea0fe78bf54aa8b901138f4
darshbot <mapadarshni@gmail.com>
2022-07-06 09:42:59 -0700

    initial commit

---
+++ Variables_Exercise/Program.cs
@@ -0,0 +1,14 @@
+﻿// Variable declerations
+int age;
+string name;
+bool isAlive;
+
+// Variable initialization
+age = 16;
+name = "Darsh";
+isAlive = true;
+
+string fullName = "Darshni Rawat";
+int houseNumber = "90"
+
+Console.WriteLine($"{name}{age} is he alive {isAlive}");
